5b6a7fd8626337232b1b509883a0cfef83ce7768
Armando <jarodriguezvera@gmail.com>
2025-05-09 20:55:07 -0600

    Agregar archivos de proyecto.

---
+++ Ordenamiento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace AlgoritmosOrdenamientoYBusqueda_2025_II
+{
+    public class Ordenamiento
+    {
+
+        public static void BubbleDesc(int [] v)
+        {
+
+            // 2 4 1 20 0 -4 9
+            
+
+            for (int i=0; i < v.Length; i++  )
+            {
+                for (int j = 0; j < v.Length - 1; j++)
+                {
+                    int aux = 0;
+                    if (v[j] < v[j + 1])
+                    {
+                        aux = v[j];
+                        v[j] = v[j + 1];
+                        v[j + 1] = aux;
+
+                    }
+                }
+            }
+
+        }
+
+
+
+    }
+}
