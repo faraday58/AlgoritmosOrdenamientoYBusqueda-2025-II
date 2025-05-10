199c877d3c0f24851edac58c5b75adef686a9f7d
Armando <jarodriguezvera@gmail.com>
2025-05-09 21:38:45 -0600

    Add vector generator

---
+++ Busqueda.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace AlgoritmosOrdenamientoYBusqueda_2025_II
+{
+    public class Busqueda
+    {
+        public static int Directa(int[] v, int x)
+        {
+            for (int i= 0; i < v.Length; i++)
+            {
+                if (v[i] == x)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AlgoritmosOrdenamientoYBusqueda_2025_II;
+
+int [] v = { 2, 4, 1, 20, 0, -4, 9 };
+
+PrintVector(v);
+Ordenamiento.BubbleDesc(v);
+PrintVector(v);
+
+
+Console.WriteLine("Ingrese un número a buscar: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int pos = Busqueda.Directa(v, x);
+
+if (pos == -1)
+{
+    Console.WriteLine("No se encontró el número");
+}
+else
+{
+    Console.WriteLine($"El número {x} está en la posición {pos} ");
+}
+
+        
+
+Console.ReadLine();
+
+
+static int [] GenerateVector(int n)
+{
+    Random random = new Random();
+    int[] v = new int[n];
+
+    for(int i = 0; i < n; i++)
+    {
+        v[i] = (int)random.NextDouble()*1000;
+    }
+    return v;
+}
+
+
+
+static void PrintVector(int [] v)
+{
+    Console.WriteLine("Vector: ");
+
+    foreach(int element in v)
+    {
+        Console.Write($" {element} ");
+    }
+    Console.WriteLine();
+} 
